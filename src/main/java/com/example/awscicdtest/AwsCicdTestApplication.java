b2bd63f4422290c6af3d0b8432e814366578cebe
Muhammad Mansoor <147248391+BzCod3r96@users.noreply.github.com>
2024-03-02 15:33:12 +0100

    1st commit

---
+++ src/main/java/com/example/awscicdtest/AwsCicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCicdTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awscicdtest/AwsCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
